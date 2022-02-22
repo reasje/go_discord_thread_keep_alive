b845cf958f864dbf61ea50dfbc2ec0b1ea7aa782
reasje <rezaasjeddian@gmail.com>
2022-02-22 18:35:43 +0330

    first succesfull test has been done

--- bot/bot.go
+++ bot/bot.go
@@ -3,8 +3,8 @@ package bot
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
+	// "os"
+	// "os/signal"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/reasje/go_discord_thread_saver/config"
@@ -17,7 +17,8 @@ var goBot *discordgo.Session
 func Start() {
 
     //creating new bot session
-	goBot, err := discordgo.New("Bot " + config.Token)
+	var err error
+	goBot, err = discordgo.New("Bot " + config.Token)
 
 //Handling error
 	if err != nil {
@@ -25,10 +26,10 @@ func Start() {
 		return
 	}
 
-	
 
+	// opens a connection to discord
 	err = goBot.Open()
-//Error handling
+	//Error handling
 	if err != nil {
 		fmt.Println(err.Error())
 		return
@@ -37,46 +38,33 @@ func Start() {
 	fmt.Println("Bot is running !")
 }
 
-func SendMessage(c chan int) {
+func SendMessage() {
 	for _ , thread := range entity.Threads {
+		// goBot.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) { fmt.Println("Bot is ready") })
 		fmt.Println(thread.ID)
-		_ , err := goBot.ChannelMessageSend(thread.ID, "Whatever , should be deleted ")
+		messsage , err := goBot.ChannelMessageSend(thread.ID, "Whatever , should be deleted ")
 
 		if err != nil {
 			fmt.Println("Error sending message")
 			return
 		}
 
-		goBot.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsAllWithoutPrivileged)
-		err = goBot.Open()
+		// goBot.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsAllWithoutPrivileged)
+		// err = goBot.Open()
 		if err != nil {
 			log.Fatalf("Cannot open the session: %v", err)
 		}
-		defer goBot.Close()
-		stop := make(chan os.Signal, 1)
-		signal.Notify(stop, os.Interrupt)
-		<-stop
+		// defer goBot.Close()
+		// stop := make(chan os.Signal, 1)
+		// signal.Notify(stop, os.Interrupt)
+		// <-stop
 
-		// err = goBot.ChannelMessageDelete(thread.ID, messsage.ID)
+		err = goBot.ChannelMessageDelete(thread.ID, messsage.ID)
 
-		// if err != nil {
-		// 	fmt.Println("Error deleting message")
-		// 	return
-		// }
+		if err != nil {
+			fmt.Println("Error deleting message")
+			return
+		}
 
-	}
-	c <- 1
-}
-
-//Definition of messageHandler function it takes two arguments first one is discordgo.Session which is s , second one is discordgo.MessageCreate which is m.
-func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	
-    //Bot musn't reply to it's own messages , to confirm it we perform this check.
-	if m.Author.ID == BotId {
-		return
-	}
-  //If we message ping to our bot in our discord it will return us pong .
-	if m.Content == "ping" {
-		_, _ = s.ChannelMessageSend(m.ChannelID, "pong")
 	}
 }
--- main.go
+++ main.go
@@ -2,53 +2,44 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
+	"time"
 
 	"github.com/reasje/go_discord_thread_saver/entity"
-	"time"
+
 	// "log"
 	// "os"
 	// "os/signal"
-	"github.com/bwmarrin/discordgo"
-	// "github.com/reasje/go_discord_thread_saver/bot"
-	// "github.com/reasje/go_discord_thread_saver/config"
+	// "github.com/bwmarrin/discordgo"
+	"github.com/reasje/go_discord_thread_saver/bot"
+	"github.com/reasje/go_discord_thread_saver/config"
 )
 
-func backgroundTask() {
+func threadKeepAliveBackgroundTask() {
 	ticker := time.NewTicker(5 * time.Second)
 	for _ = range ticker.C {
-		fmt.Println("Tick at")
+
+		// reading threads from file
 		err := entity.ReadThreads()
 
 		if err != nil {
 			fmt.Println("Error reading threads ")
 			return
 		}
-		s, err := discordgo.New("Bot " + "OTQ1MjMyODc2NjQxODY5ODM0.YhNKtw.4zt-cAvzBa9h65tQDCjmrX9dPd8")
-		
-		if err != nil {
-			fmt.Println("Error sending message")
-		}
 
-		s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) { fmt.Println("Bot is ready") })
-		message, err := s.ChannelMessageSend("945225329377697873", "Hello, world!")
-		if err != nil {
-			fmt.Println("Error sending message")
+		bot.SendMessage()
+	}
+}
+
+func getUserResfreshBackgroundTask() {
+	for {
+		var userInput string
+		fmt.Scanln(&userInput)
+		userInput = strings.ToLower(userInput)
+		if userInput == "r" {
+			fmt.Println("List of games:")
 		}
-		print(message)
-		// s.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsAllWithoutPrivileged)
-		// err = s.Open()
-		// if err != nil {
-		// 	log.Fatalf("Cannot open the session: %v", err)
-		// }
-		// defer s.Close()
-		// stop := make(chan os.Signal, 1)
-		// signal.Notify(stop, os.Interrupt)
-		// <-stop
-		// log.Println("Graceful shutdown")
-		// sendMessageChannel := make(chan int)
-		// go bot.SendMessage(sendMessageChannel)
-		// <-sendMessageChannel
 	}
 }
 
@@ -63,18 +54,19 @@ func main() {
 	}
 
 
-	// err = config.ReadConfig()
+	err = config.ReadConfig()
 
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	return
-	// }
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
-	// bot.Start()
+	bot.Start()
 
-	go backgroundTask()
+	// keeping the task in the background up and running
+	go threadKeepAliveBackgroundTask()
 
-
+	go getUserResfreshBackgroundTask()
 
 
 	// go func() {
